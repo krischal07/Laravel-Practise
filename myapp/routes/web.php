91ec66272c5a6f36387166474a90d06885d183cb
krischal07 <krischal.shrestha9849@gmail.com>
2024-11-27 14:39:40 +0545

    laravel day 3

---
+++ myapp/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    // return view('welcome');
+    echo "<h1>Hello Krischal this is first laravel page</h1>";
+});
+
+Route::get('/about-us', function () {
+    // return view('welcome');
+    echo "<h1>ABout us page</h1>";
+});
